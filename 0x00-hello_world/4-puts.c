0145057a8922f2a8c95d7466f7f9b0eb9f17a50d
Rubaya Christopher <rbcrys@gmail.com>
2022-09-12 11:43:15 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 /**
- * main prints output "Programming is like building a multilingual puzzle"
- * program ends with value 0 if exited properly, otherwise non-zero
+ * main - prints output "Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, otherwise non-zero
  */
 int main(void)
 {
 	puts("\""Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
