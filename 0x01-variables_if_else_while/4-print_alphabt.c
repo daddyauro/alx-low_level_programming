41a04911adfa58d2bbe0a825a2d96ca115f0c46f
Rubaya Christopher <rbcrys@gmail.com>
2022-09-13 02:36:12 -0700

    excluding while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ * except q and e
+ * Return: 0 (Always success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
