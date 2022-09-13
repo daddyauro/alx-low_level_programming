dd25f8962d585bf92c46dced477d5b4e0a7376a5
Rubaya Christopher <rbcrys@gmail.com>
2022-09-13 01:58:57 -0700

    alpha c C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowecase
+ * then uppercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
