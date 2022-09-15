fcf3ec292122418cb4b30dbdb6a031fdbc5fed6c
Rubaya Christopher <rbcrys@gmail.com>
2022-09-15 05:23:40 -0700

    interger added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers and
+ * returns result
+ *@n: interger to be added
+ *@m: interger to be added
+ * Return: products
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
+
