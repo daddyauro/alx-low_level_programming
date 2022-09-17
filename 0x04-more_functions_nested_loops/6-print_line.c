5631b22c20dc774e2016f1913159dca4034fa42c
Rubaya Christopher <rbcrys@gmail.com>
2022-09-17 14:45:12 -0700

    yes to all

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * * print_line - Draws a straight line using the character
+ * @n: The number of _ characters to be printed
+ */
+void print_line(int n)
+{
+	int m;
+
+	if (n > 0)
+	{
+		for (m = 0; m < n; m++)
+			_putchar ('_');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *  main - Prints the numbers from 1-100, but for multiples of three,
+ *  Fizz is printed instead of the number, for multiples of five,
+ *  Return: Always 0 success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+			printf("FizzBuzz");
+		else if ((n % 3) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", n);
+		if (n == 100)
+			continue;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
