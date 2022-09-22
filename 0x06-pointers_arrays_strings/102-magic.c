29e686a20d783e301a186d29f5668c4c774c7dad
Rubaya Christopher <rbcrys@gmail.com>
2022-09-22 07:44:19 -0700

    pointers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include <stdio.
 int main(void)
 {
 	int n;
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,18 +7,19 @@
  */
 char *leet(char *str)
 {
-	int i = 0, j;
+	int indx1 = 0, indx2;
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (str[j])
+	while (str[indx1])
 	{
-		for (j = 0; j <= 7; j++)
+		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
-			if (str[i] == leet[j] || str[i] - 32 == leet[j])
-				str[i] = j + '0';
+			if (str[indx1] == leet[indx2] ||
+					str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
 		}
 
-		j++;
+		indx1++;
 	}
 
 	return (str);
