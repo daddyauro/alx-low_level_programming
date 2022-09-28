60a39167c07af4c14ac845a39222821a943dcb4d
Rubaya Christopher <rbcrys@gmail.com>
2022-09-28 12:29:15 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: string length to be measured
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+	return (0);
+s++;
+return (1 + _strlen_recursion(s));
+}
